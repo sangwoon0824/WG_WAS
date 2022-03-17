52655bc443681537f4dccb6a4431e9b89505f887
sangwoon0824 <sangwoon0824@naver.com>
2022-03-17 22:13:38 +0900

    test

--- public/_js/weatherballoon.js
+++
@@ -1,7 +0,0 @@
-const weather = ["sun","rain","cloud","thunder","snow"];
-
-document.getElementById("balloon-1").src = "_img/about/balloon/balloon_" + weather[Math.floor(Math.random() * 5)] + ".png";
-document.getElementById("balloon-2").src = "_img/about/balloon/balloon_" + weather[Math.floor(Math.random() * 5)] + ".png";
-document.getElementById("balloon-3").src = "_img/about/balloon/balloon_" + weather[Math.floor(Math.random() * 5)] + ".png";
-document.getElementById("balloon-4").src = "_img/about/balloon/balloon_" + weather[Math.floor(Math.random() * 5)] + ".png";
-document.getElementById("balloon-5").src = "_img/about/balloon/balloon_" + weather[Math.floor(Math.random() * 5)] + ".png";
