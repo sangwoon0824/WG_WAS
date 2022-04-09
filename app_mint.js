b2ad3fc29999774c0954bd8ce9ec0a4af11dfde0
sangwoon0824 <sangwoon0824@naver.com>
2022-04-10 04:53:03 +0900

    test

--- app_mint.js
+++ app_mint.js
@@ -2,10 +2,8 @@
 const express = require("express");
 const app = express();
 const router = express.Router();
-//const helmet = require("helmet");
-const limit = require("express-rate-limit");
-const bodyParser = require("body-parser");
 const fs = require("fs");
+//const helmet = require("helmet");
 const limit = require("express-rate-limit");
 const Caver = require("caver-js");
 const CONTRACT = require("./build/wgContract.json");
@@ -26,30 +24,7 @@ const acc = caver.klay.accounts.wallet.getAccount(0);
 
 const networkID = "1001";
 const contract = new caver.klay.Contract(CONTRACT.abi, CONTRACT.address);
-/*
-//basic node security
-const cspOptions = {
-  directives: {
-    // 기본 옵션을 가져옵니다.
-    ...helmet.contentSecurityPolicy.getDefaultDirectives(),
 
-    "script-src": [
-      "'self'",
-      "*.googleapis.com",
-      "https://hangeul.pstatic.net",
-      "https://cdn.jsdelivr.net",
-    ],
-  },
-};
-
-
-// Helmet의 모든 기능 사용. (contentSecurityPolicy에는 custom option 적용)
-app.use(
-  helmet({
-    contentSecurityPolicy: cspOptions,
-  })
-);
-*/
 //hide backend engine
 app.disable("x-powered-by");
 
@@ -62,39 +37,16 @@ app.use(
 );
 
 //포트 설정
-const port = process.env.PORT || 8080;
+const port = process.env.PORT || 8888;
 
 //테스트 서버 포트
 //const port = process.env.PORT || 800;
-
+let round = 0;
 //동적 폴더(CSS,JS 로딩 용이)
 app.use(express.static(__dirname + "/public"));
 
 app.get("/", (req, res) => {
   res.render("index.html");
-});
-
-app.get("/metadata/:id", (req, res) => {
-  let id = req.params.id;
-  if (id > 0) {
-    res.send(
-      "<script>alert('발행되지않은 토큰입니다');</script>\n" +
-        "<div>non-existent token ID</div>"
-    );
-  } else {
-    res.sendFile(__dirname + "/json/" + id + ".json");
-  }
-});
-
-app.get("/", (req, res) => {
-  res.render("index.html");
-});
-app.get("/mint", (req, res) => {
-  res.render("mint.html");
-});
-
-app.get("/not-support-this-browser", (req, res) => {
-  res.sendFile(__dirname + "/public/not-support-this-browser.html");
 });
 
 app.post("/checkwhitelist", (req, res) => {
