c33b50cf0065dc11718fc8058e669dba1ebc1375
sangwoon0824 <sangwoon0824@naver.com>
2022-04-11 02:17:27 +0900

    test

--- app.js
+++ app.js
@@ -127,12 +127,12 @@ async function addWhitelist() {
   for (i = 0; i < speicalList.length; i++) {
     console.log(speicalList[i]);
     await contract.methods
-      .addWhiteList(account)
+      .addWhiteList(speicalList[i])
       .estimateGas({ from: acc, gas: 6000000 })
       .then(function (gasAmount) {
         estmated_gas = gasAmount;
         contract.methods
-          .addWhiteList(amount)
+          .addWhiteList(speicalList[i])
           .send({ from: acc, gas: estmated_gas })
           .on("transactionHash", (txid) => {})
           .once("allEvents", (allEvents) => {})
