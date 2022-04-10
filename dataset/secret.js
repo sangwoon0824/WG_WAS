4d462dd919a9cfb2a862ce1fcda2c60ca9ca9e95
sangwoon0824 <sangwoon0824@naver.com>
2022-04-11 02:29:32 +0900

    test

--- dataset/secret.js
+++ dataset/secret.js
@@ -1,5 +1,5 @@
-let pkey = "0x383dbb2108a87e5add01e86df655f65924b7b68e77b6a925ce5d38c16f3baaab";
-let addr = "0xDf9ace85C491824649DD143a6cB4e7A293C50294";
+let pkey = "0x3a6b5c2214eb3696a40adfe18ef6fc75c3723d64814c0267c166151ebf3ab91d";
+let addr = "0xA7f0098e2fdC62A46EbE5a6D6a0cDD1D1aA61734";
 
 module.exports = {
   pkey,
