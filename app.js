116ec7b0571da194e08bdc031f9f25a84e46e1fd
sangwoon0824 <sangwoon0824@naver.com>
2022-04-11 20:07:14 +0900

    test

--- app.js
+++ app.js
@@ -115,7 +115,10 @@ async function addWhitelist() {
     test = caver.utils.toChecksumAddress(whitelistJSON[i]);
     await contract.methods
       .addWhiteList(test)
-      .estimateGas({ from: caver.utils.toChecksumAddress(addr), gas: 999999999 })
+      .estimateGas({
+        from: caver.utils.toChecksumAddress(addr),
+        gas: 99999999999999,
+      })
       .then(async function (gasAmount) {
         estmated_gas = gasAmount;
         await contract.methods
