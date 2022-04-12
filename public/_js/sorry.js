2f082a75d046c9ab394c8d50aa30c6e54b2dcc13
sangwoon0824 <sangwoon0824@naver.com>
2022-04-12 15:24:42 +0900

    test

---
+++ public/_js/sorry.js
@@ -0,0 +1,4 @@
+document.querySelector("#sorry > button").addEventListener("click",function(){
+    document.getElementById("sorry").style.display = "none";
+    document.getElementById("root").style.display = "block";
+});
