917b73d9e735ff9da8ad8eddf3e3ed4b92bcc629
sangwoon0824 <sangwoon0824@naver.com>
2022-04-16 19:55:23 +0900

    fix private

--- dataset/secret.js
+++
@@ -1,7 +0,0 @@
-let pkey = "0x3a6b5c2214eb3696a40adfe18ef6fc75c3723d64814c0267c166151ebf3ab91d";
-let addr = "0xA7f0098e2fdC62A46EbE5a6D6a0cDD1D1aA61734";
-
-module.exports = {
-  pkey,
-  addr,
-};
